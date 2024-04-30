902dcdbc788487dd031a28dd0b34bf34634cc132
rashed <rasheduzzaman@ba-systems.com>
2024-04-30 15:36:06 +0600

    file upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import PyPDF2
+
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        num_pages = len(reader.pages)
+        for page_num in range(num_pages):
+            page = reader.pages[page_num]
+            text += page.extract_text()
+    return text
+
+# Example usage:
+pdf_path = ""
+extracted_text = extract_text_from_pdf(pdf_path)
+print(extracted_text)
